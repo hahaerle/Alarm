94b7902504198e4851802eb2d15150c384aa3502
shangerle <shangerle@51yche.com>
2015-07-13 18:17:00 +0800

    test modify

--- app/src/main/java/com/lenote/alarmstar/activity/BaseActivity.java
+++ app/src/main/java/com/lenote/alarmstar/activity/BaseActivity.java
@@ -3,7 +3,7 @@ package com.lenote.alarmstar.activity;
 import android.support.v7.app.ActionBarActivity;
 
 /**
- * Created by shanerle on 2015/7/13.
+ * Created by lenote on 2015/7/13.
  */
 public class BaseActivity extends ActionBarActivity {
 }
